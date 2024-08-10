044799d18fba57d314e8100ad543a104e62a1a06
leayaaah <23711991.thuy@student.iuh.edu.vn>
2024-08-10 23:44:23 +0700

    hi

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Ly Thi Thuy!");
+        System.out.println("Welcome to DHKTPM19B!");
+    }
+}
